cb6407c545b123b6ac8e56671a5aafe9daabed61
Adrian Vatchinsky <avatchinsky@outlook.com>
2016-08-30 08:59:30 -0400

    sync on beforeEnter event

--- www/js/app.js
+++ www/js/app.js
@@ -364,9 +364,60 @@ angular.module('relish', ['ionic', 'LocalStorageModule', 'monospaced.qrcode'])
     }
   }
   checkActionBtnState();
-  
 
-  function syncStudy(){
+  function showCoupon(){
+    $scope.isPriming = false;
+  }
+  $scope.showCoupon = showCoupon;
+
+  function reset(){
+    console.log('click');
+    $scope.inRegion = false;
+    $scope.isPriming = true;
+    checkActionBtnState();
+  }
+  $scope.reset = reset;
+
+
+  function getCoords(){
+    var deferred = $q.defer();
+    $ionicPlatform.ready(function(){
+      Geolocation.getCurrentPosition()
+        .then(function(position){
+          deferred.resolve({
+            lat: position.coords.latitude,
+            lng: position.coords.longitude
+          });
+        }, function(e) {
+          // error
+          console.log(e);
+          // deferred.reject(e);
+          deferred.resolve({"lat":40.740999620828084,"lng":-74.00181926300179})
+        });
+    });
+    
+    return deferred.promise;
+  }
+
+  function getDistance(lat1,lon1,lat2,lon2) {
+    var R = 6371; // Radius of the earth in km
+    var dLat = deg2rad(lat2-lat1);  // deg2rad below
+    var dLon = deg2rad(lon2-lon1); 
+    var a = Math.sin(dLat/2) * Math.sin(dLat/2) +
+            Math.cos(deg2rad(lat1)) * Math.cos(deg2rad(lat2)) * 
+            Math.sin(dLon/2) * Math.sin(dLon/2);
+    var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a)); 
+    var d = R * c * 1000; // Distance in m
+
+    return d;
+  }
+
+  function deg2rad(deg) {
+    return deg * (Math.PI/180)
+  }
+
+
+  function sync(){
     StudyService.loadStudies()
       .then(function(study){
         console.log("Got the study");
@@ -432,58 +483,13 @@ angular.module('relish', ['ionic', 'LocalStorageModule', 'monospaced.qrcode'])
         alert("Failed to sync study");
       });
   }
-  syncStudy();
-
-  function showCoupon(){
-    $scope.isPriming = false;
-  }
-  $scope.showCoupon = showCoupon;
-
-  function reset(){
-    console.log('click');
-    $scope.inRegion = false;
-    $scope.isPriming = true;
-    checkActionBtnState();
-  }
-  $scope.reset = reset;
 
 
-  function getCoords(){
-    var deferred = $q.defer();
-    $ionicPlatform.ready(function(){
-      Geolocation.getCurrentPosition()
-        .then(function(position){
-          deferred.resolve({
-            lat: position.coords.latitude,
-            lng: position.coords.longitude
-          });
-        }, function(e) {
-          // error
-          console.log(e);
-          // deferred.reject(e);
-          deferred.resolve({"lat":40.740999620828084,"lng":-74.00181926300179})
-        });
-    });
-    
-    return deferred.promise;
-  }
-
-  function getDistance(lat1,lon1,lat2,lon2) {
-    var R = 6371; // Radius of the earth in km
-    var dLat = deg2rad(lat2-lat1);  // deg2rad below
-    var dLon = deg2rad(lon2-lon1); 
-    var a = Math.sin(dLat/2) * Math.sin(dLat/2) +
-            Math.cos(deg2rad(lat1)) * Math.cos(deg2rad(lat2)) * 
-            Math.sin(dLon/2) * Math.sin(dLon/2);
-    var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a)); 
-    var d = R * c * 1000; // Distance in m
-
-    return d;
-  }
-
-  function deg2rad(deg) {
-    return deg * (Math.PI/180)
-  }
+  $scope.$on("$ionicView.beforeEnter", function(event, data){
+    // handle event
+    console.log("ENTER");
+    sync();
+  });
 
 })
 
